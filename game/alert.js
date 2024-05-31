92f29e980ed41a5b3d82f330a793ba9698ec94ad
lang0502 <153420127+lang0502@users.noreply.github.com>
2024-05-31 15:29:37 +0700

    Add files via upload

---
+++ game/alert.js
@@ -0,0 +1,9 @@
+var popupconfirm = confirm("Mau nunggu ga ngab? banyak source jadi agak lama paling 30 detik")
+if(popupconfirm === true)
+{
+       alert("Oke ngab gas!");
+}
+else{
+       alert("Cuihh!! ga sabaran banget lu!");
+
+}
